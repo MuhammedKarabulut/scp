d3f35bdfbff4871784b9938d410837e8fd217d13
olhokom <116507233+olhokom@users.noreply.github.com>
2023-02-19 20:06:42 +0300

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,8 +1,14 @@
-sudo apt-get install sshpass
-###_SLEEP:3000
+apt install sshpass
+
+sleep 4
+sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r  root@5.161.53.96:/root/AutoRclone/autoClone/autoClone.sh /root/AutoRclone/autoClone
+
+sleep 4
 sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r root@5.161.53.96:/root/.config/rclone/rclone.conf /root/.config/rclone/rclone.conf
-sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r root@5.161.53.96:/root/AutoRclone/autoClone/autoClone.sh /root/AutoRclone/autoClone
-###_SLEEP:3000
-screen -rd clone
-{#_^C}
+
+sleep 4
+ screen -S clone -X stuff  "^C ^M"
+
+sleep 4
+
 /root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 200 22
