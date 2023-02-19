c07ad5346581c10cc4a09ee56dc9045709981244
olhokom <116507233+olhokom@users.noreply.github.com>
2023-02-19 20:12:03 +0300

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -8,8 +8,6 @@ sleep 4
 sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r root@5.161.53.96:/root/.config/rclone/rclone.conf /root/.config/rclone/rclone.conf
 
 sleep 4
-screen -S clone -X stuff  "^C ^M"
+ screen -S clone -X stuff  "^C ^M"
 
-sleep 2
-
-/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 200 22
+ screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 200 22 ^M"
