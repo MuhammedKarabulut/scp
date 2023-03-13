3ca8a98bc4f882c3780e6b5968d26ef102df5894
MuhammedKarabulut <113688916+MuhammedKarabulut@users.noreply.github.com>
2023-03-14 00:09:57 +0300

    Update ss2.sh

--- ss2.sh
+++ ss2.sh
@@ -34,17 +34,4 @@ chmod 777 /root/AutoRclone/autoClone/madmax_ram.sh
 chmod 777 /root/AutoRclone/autoClone/madmax_bld.sh
 chmod 777 /root/AutoRclone/autoClone/autoClone.sh
 screen -dmS madmax
-screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh UP 0 200 22 ^M"
-
-apt install sshpass
-
-sleep 4
-sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r  root@5.161.53.96:/root/AutoRclone/autoClone/autoClone.sh /root/AutoRclone/autoClone
-
-sleep 4
-sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r root@5.161.53.96:/root/.config/rclone/rclone.conf /root/.config/rclone/rclone.conf
-
-sleep 4
- screen -S clone -X stuff  "^C ^M"
-
- screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 200 22 ^M"
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh UP 0 200 24 ^M"
